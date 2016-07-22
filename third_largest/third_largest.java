b751be6f81c23d5909861a2e8a96c5635977af56
johnngugi <johnngugi777@gmail.com>
2016-07-22 08:59:23 +0300

    Solution for third largest problem

--- third_largest/third_largest.java
+++ third_largest/third_largest.java
@@ -1,6 +1,44 @@
-// Using the Java language, have the function ThirdGreatest(strArr)
-// take the array of strings stored in strArr and return the third largest word within in.
-// So for example: if strArr is ["hello", "world", "before", "all"]
-// your output should be "world" because "before" is 6 letters long, and "hello" and "world" are both 5, but the output should be world because it appeared as the last 5 letter word in the array. If strArr was ["hello", "world", "after", "all"] the output should be after because the first three words are all 5 letters long, so return the last one. The array will have at least three strings and each string will only contain letters.
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package toyproblem1;
 
-// use the main method only for calling the function where your logic lies. :-)
+/**
+ *
+ * @author john
+ */
+import java.util.Arrays;
+public class ToyProblem1 {
+
+    /**
+     * @param array
+     * @return 
+     */
+    
+    public static String arr(String[] array) {
+        boolean sort = true;
+        String value = "";
+        while(sort) {
+            for (int i = 0; i < array.length-1; i++) {
+                if(array[i].length() < array[i+1].length()) {
+                    value = array[i];
+                    array[i] = array[i+1];
+                    array[i+1] = value;
+                }
+                
+            }
+            sort = false;
+        }
+        return value;
+    }
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String[] s = {"petr", "johnkarush", "simon"};
+        arr(s);
+        int a = s.length-1;
+        System.out.println(s[a]);
+    }
+    
+}

429af952ac83327f1ca43baac5c01be27885804c
johnngugi <johnngugi777@gmail.com>
2016-07-25 08:28:51 +0300

    Add solution to toy problem

--- maximum_and_minimum/Maximum_minimum.java
+++ maximum_and_minimum/Maximum_minimum.java
@@ -9,3 +9,39 @@
 // put the lowest in its array 
 // put the highest in its array
 // output the highest then output the lowest
+
+import java.util.Arrays;
+public class Maximum_Minimum {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static int[] sortArray(int[] array) {
+        int list = 0;
+        
+        for(int i = 0;i < array.length-1;i++) {
+            for(int j =0;j<array.length-1;j++) {
+                if(array[j] > array[j+1]) {
+                    list = array[j+1];
+                    array[j+1] = array[j];
+                    array[j] = list;
+                }
+            }
+        }
+        return array;
+    }
+    
+        
+    
+    public static void main(String[] args) {
+        int[] numbers = { 5, 8, 14, 1, 5678, -568};
+        sortArray(numbers);
+        int[] max = {numbers[numbers.length-1]};
+        int[] min = {numbers[0]};
+        
+        System.out.println(Arrays.toString(min));
+        System.out.println(Arrays.toString(max));
+        
+    }
+    
+}
